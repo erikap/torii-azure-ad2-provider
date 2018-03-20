4be140c662fc04a888f0816905ac279695c23bb8
Erika Pauwels <erika.pauwels@gmail.com>
2018-03-20 21:23:16 +0100

    Add Azure AD v2 torii provider

---
+++ addon/torii-providers/azure-ad2-oauth2.js
@@ -0,0 +1,32 @@
+import Oauth2 from 'torii/providers/oauth2-code';
+import { configurable } from 'torii/configuration';
+import { computed } from '@ember/object';
+
+/**
+ * This class implements authentication against Azure AD 2.0
+ * using the OAuth2 authorization flow in a popup window.
+ */
+export default Oauth2.extend({
+  name: 'azure-ad2-oauth2',
+
+  baseUrl: computed('tenantId', function() {
+    return `https://login.microsoftonline.com/${this.get('tenantId')}/oauth2/v2.0/authorize`;
+  }),
+
+  init() {
+    this.set('requiredUrlParams', ['response_type', 'client_id', 'redirect_uri', 'state', 'scope']);
+    this.set('optionalUrlParams', ['response_mode', 'prompt', 'login_hint', 'domain_hint']);
+    this.set('responseParams', ['code', 'state']);
+  },
+
+  tenantId: configurable('tenantId', 'common'),
+  scope: configurable('scope', 'offline_access User.Read'),
+  responseMode: configurable('responseMode', 'query'),
+  prompt: configurable('prompt', null),
+  login_hint: configurable('login_hint', null),
+  domain_hint: configurable('domain_hint', null),
+
+  redirectUri: configurable('redirectUri', function() {
+    return this._super();
+  })
+});
---
+++ app/torii-providers/azure-ad2-oauth2.js
@@ -0,0 +1,3 @@
+import AzureAd2OAuth2 from 'torii-azure-ad2-provider/torii-providers/azure-ad2-oauth2';
+
+export default AzureAd2OAuth2;
